a7bf6c7e068bedfab4717edf322157904d1054c6
WeiLiang <511684406@qq.com>
2018-02-14 16:38:54 +0900

    you can add some sofas

--- Main.java
+++ Main.java
@@ -1,8 +1,9 @@
 public class Main {
     private PApplet processing;
     private PImage backgroundImage;
-    private Bed[] beds;
+    private Furniture[] furni;
     private CreateBedButton bedButton;
+    private CreateSofaButton sofaButton;
 
     public Main(PApplet processing) {
         this.processing = processing;
@@ -25,13 +26,16 @@ public class Main {
         backgroundImage = processing.loadImage("images/background.png");
         processing.image(backgroundImage, processing.width/2, processing.height/2);
 
-        // Bed
-        beds = new Bed[6];
-        for( Bed bed : beds )
-            bed = null;
+        // Furniture
+        furni = new Furniture[6];
+        for( Furniture fur : furni)
+            fur = null;
 
         // Bed button
         bedButton = new CreateBedButton(50,24,processing);
+
+        // Sofa Button
+        sofaButton = new CreateSofaButton(150,24,processing);
     }
 
     public void update(){
@@ -45,21 +49,48 @@ public class Main {
         processing.image(backgroundImage, processing.width/2, processing.height/2);
 
         // Update each bed
-        for( int i=0; i<beds.length; i++){
-            if( beds[i] != null )
-                beds[i].update();
+        for(int i = 0; i< furni.length; i++){
+            if( furni[i] != null )
+                furni[i].update();
         }
 
         // Update bed button
         bedButton.update();
+
+        // Update sofa button
+        sofaButton.update();
     }
 
 
     public void mouseDown(){
         //System.out.println("mouse down");
-        for( int i=beds.length-1; i>=0; i--){
-            if( beds[i] != null && beds[i].isMouseOver() ){
-                beds[i].mouseDown();
+        if( bedButton.isMouseOver() ){
+            for(int i = 0; i< furni.length; i++ ){
+                if( furni[i] == null ){
+                   Furniture bed = bedButton.mouseDown();
+                   if( bed != null )    // If it's not null
+                       furni[i] = bed;
+                   break;
+                }
+            }
+            return;
+        }
+
+        if( sofaButton.isMouseOver() ){
+            for(int i = 0; i< furni.length; i++ ){
+                if( furni[i] == null ){
+                    Furniture sofa = sofaButton.mouseDown();
+                    if( sofa != null )    // If it's not null
+                        furni[i] = sofa;
+                    break;
+                }
+            }
+            return;
+        }
+
+        for(int i = furni.length-1; i>=0; i--){
+            if( furni[i] != null && furni[i].isMouseOver() ){
+                furni[i].mouseDown();
                 return;
             }
         }
@@ -67,9 +98,9 @@ public class Main {
 
     public void mouseUp() {
         //System.out.println("mouse up");
-        for( int i=0; i<beds.length; i++){
-            if( beds[i] != null && beds[i].isMouseOver() ){
-                beds[i].mouseUp();
+        for(int i = 0; i< furni.length; i++){
+            if( furni[i] != null && furni[i].isMouseOver() ){
+                furni[i].mouseUp();
             }
         }
     }
@@ -77,30 +108,20 @@ public class Main {
     public void keyPressed(){
         //System.out.println("keyPressed");
         switch(processing.key){
-            case 'b':
-            case 'B':
-                for( int i=0; i<beds.length; i++){
-                    if( beds[i] == null ){
-                        beds[i] = new Bed(processing);
-                        beds[i].update();
-                        return;
-                    }
-                }
-                break;
             case 'd':
             case 'D':
-                for( int i=beds.length-1; i>=0; i--){
-                    if( beds[i] != null && beds[i].isMouseOver() ){
-                        beds[i] = null;
+                for(int i = furni.length-1; i>=0; i--){
+                    if( furni[i] != null && furni[i].isMouseOver() ){
+                        furni[i] = null;
                         return;
                     }
                 }
                 break;
             case 'r':
             case 'R':
-                for( int i=beds.length-1; i>=0; i--){
-                    if( beds[i] != null && beds[i].isMouseOver() ) {
-                        beds[i].rotate();
+                for(int i = furni.length-1; i>=0; i--){
+                    if( furni[i] != null && furni[i].isMouseOver() ) {
+                        furni[i].rotate();
                         return;
                     }
                 }
@@ -110,76 +131,4 @@ public class Main {
     }
 }
 
-class Bed{
-    private PApplet processing;
-    private PImage image;
-    private float[] position;
-    private boolean isDragging;
-    private float deltaX;
-    private float deltaY;
-    private int rotations;
 
-    // initializes the fields of a new bed object positioned in the center of the display
-    public Bed(PApplet processing) {
-        this.processing = processing;
-        image = processing.loadImage("images/bed.png");
-        position = new float[2];
-        position[0] = processing.width/2;
-        position[1] = processing.height/2;
-        isDragging = false;
-
-        deltaX = 0;
-        deltaY = 0;
-
-        rotations = 0;
-    }
-
-    // draws this bed at its current position
-    public void update() {
-        if( isDragging ){
-            position[0] = processing.mouseX + deltaX;
-            position[1] = processing.mouseY + deltaY;
-        }
-        processing.image(image, position[0], position[1], rotations*PApplet.PI/2);
-    }
-
-    // used to start dragging the bed, when the mouse is over this bed when it is pressed
-    public void mouseDown() {
-        isDragging = true;
-        deltaX = position[0] - processing.mouseX;
-        deltaY = position[1] - processing.mouseY;
-    }
-
-    // used to indicate that the bed is no longer being dragged
-    public void mouseUp() {
-        isDragging = false;
-    }
-
-    // helper method to determine whether the mouse is currently over this bed
-    public boolean isMouseOver() {
-        float halfWidth,halfHeight;
-
-        if( rotations % 2 == 0 ){
-            halfWidth = image.width/2;
-            halfHeight = image.height/2;
-        }else{
-            halfWidth = image.height/2;
-            halfHeight = image.width/2;
-        }
-
-        float judgeX = processing.mouseX - position[0];
-        if(judgeX<0)
-            judgeX = -judgeX;
-        float judgeY = processing.mouseY - position[1];
-        if(judgeY<0)
-            judgeY = -judgeY;
-        if(judgeX <= halfWidth && judgeY <= halfHeight)
-            return true;
-        else
-            return false;
-    }
-
-    public void rotate(){
-        rotations = ( rotations+1 ) % 4;
-    }
-}
